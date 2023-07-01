4e9406b3287cf00d56b0a7fec86277329b5c0f71
Abdelrahman Essam <64382556+3BDO-debug@users.noreply.github.com>
2023-07-01 18:39:47 +0300

    Small changes to equipment in workout for data entry

--- workout_plan_algorithm/models.py
+++ workout_plan_algorithm/models.py
@@ -32,10 +32,16 @@ class Muscle(models.Model):
 class Equipment(models.Model):
     name = models.CharField(max_length=350, verbose_name="Equipment name")
     related_muscle = models.ForeignKey(
-        Muscle, on_delete=models.CASCADE, verbose_name="Related muscle"
+        Muscle,
+        on_delete=models.CASCADE,
+        verbose_name="Related muscle",
+        null=True,
+        blank=True,
     )
     img = CloudinaryField(verbose_name="Equipment image", null=True, blank=True)
-    img_link = models.URLField(verbose_name="Equipment image link", null=True, blank=True)
+    img_link = models.URLField(
+        verbose_name="Equipment image link", null=True, blank=True
+    )
 
     class Meta:
         verbose_name = "Equipment"
@@ -50,12 +56,27 @@ class Excercise(models.Model):
     related_muscle = models.ForeignKey(
         Muscle, on_delete=models.CASCADE, verbose_name="Related muscle"
     )
-    related_accessory_muscle = models.ManyToManyField(Muscle,  verbose_name="Accessory muscle", related_name="related_accessory_muscle_muscle")
-    related_equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, verbose_name="Related equipment", null=True, blank=True, related_name="related_equipment")
-    workout_thumbnail  = CloudinaryField(verbose_name = "Excercise thumbnail", null=True, blank=True)
+    related_accessory_muscle = models.ManyToManyField(
+        Muscle,
+        verbose_name="Accessory muscle",
+        related_name="related_accessory_muscle_muscle",
+    )
+    related_equipment = models.ForeignKey(
+        Equipment,
+        on_delete=models.CASCADE,
+        verbose_name="Related equipment",
+        null=True,
+        blank=True,
+        related_name="related_equipment",
+    )
+    workout_thumbnail = CloudinaryField(
+        verbose_name="Excercise thumbnail", null=True, blank=True
+    )
     video = CloudinaryField(verbose_name="Excercise video", null=True, blank=True)
     video_link = models.URLField(verbose_name="Excercise video", null=True, blank=True)
-    is_home_workout = models.BooleanField(default=False, verbose_name="Is home excercise ?")
+    is_home_workout = models.BooleanField(
+        default=False, verbose_name="Is home excercise ?"
+    )
 
     class Meta:
         verbose_name = "Excercise"
@@ -63,13 +84,20 @@ class Excercise(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Injury(models.Model):
     name = models.CharField(max_length=350, verbose_name="Injury name")
-    excluded_workouts = models.ManyToManyField(Excercise, verbose_name="Excluded excercises")
+    excluded_workouts = models.ManyToManyField(
+        Excercise, verbose_name="Excluded excercises"
+    )
     level = models.CharField(max_length=350, verbose_name="Level")
-    explaination_video  = CloudinaryField(verbose_name ="Injury Explaination video", null=True, blank=True)
-    explaination_video_link  = CloudinaryField(verbose_name ="Injury Explaination video", null=True, blank=True)
+    explaination_video = CloudinaryField(
+        verbose_name="Injury Explaination video", null=True, blank=True
+    )
+    explaination_video_link = CloudinaryField(
+        verbose_name="Injury Explaination video", null=True, blank=True
+    )
     description = models.TextField(verbose_name="Injury explaination video")
 
     class Meta:
@@ -78,4 +106,3 @@ class Injury(models.Model):
 
     def __str__(self):
         return self.name
-    
